11dd3d9c0018a834a0eaaed0ed490cec16828204
Friedrich Doku <friedrichdoku@gmail.com>
2019-11-17 16:05:41 -0600

    Minor changes to vga

--- ros/src/vga_buf.rs
+++ ros/src/vga_buf.rs
@@ -53,3 +53,42 @@ pub struct Writer {
     color_code: ColorCode,
     buffer: &'static mut Buffer,
 }
+
+impl Writer {
+    pub fn write_byte(&mut self, byte: u8){
+        match byte{
+            b'\n' => self.new_line(),
+            byte => {
+                if self.column_position >= BUFFER_WIDTH {
+                    self.new_line();
+                }
+
+                let row = BUFFER_HEIGHT -1;
+                let col = self.column_position;
+
+                let color_code = self.color_code;
+                self.buffer.chars[row][col] = ScreenChar{
+                    ascii_code: byte,
+                    color_code,
+                };
+                self.column_position +=1;
+            }
+        }
+    }
+
+    //TODO: Finish this
+    pub fn new_line(&mut self){}
+
+    pub fn write_string(&mut self, s: &str){
+        for byte in s.bytes() {
+            match byte {
+                // A byte that's part of the ASCII table
+                0x20..=0x7e | b'\n' => self.write_byte(byte),
+                // else
+                _ => self.write_byte(0xfe),
+            }
+        }
+    }
+
+
+}
