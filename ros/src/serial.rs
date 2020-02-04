0d728d3e8e7fc4a5246fa526b9b53e946c296d70
professor <friedrichdoku@gmail.com>
2020-02-03 21:39:53 -0600

    Minor changes to source

--- ros/src/serial.rs
+++ ros/src/serial.rs
@@ -18,7 +18,7 @@ pub fn _print(args: ::core::fmt::Arguments){
     use core::fmt::Write;
     use x86_64::instructions::interrupts;
 
-    interrupts.without_interrups(|| {
+    interrupts::without_interrupts(|| {
         SERIAL1.lock().write_fmt(args).expect("Printing to serial failed");
     });
 }
